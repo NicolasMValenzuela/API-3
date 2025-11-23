1bfec60c6b95524a81ca38b3ea8cff125c027946
Fran Reyna <franreyna2004@gmail.com>
2025-11-22 21:28:33 -0300

    Agregue ordersSlice

--- Ecommerce-master/frontend/src/redux/store.js
+++ Ecommerce-master/frontend/src/redux/store.js
@@ -1,11 +1,20 @@
 import {configureStore} from '@reduxjs/toolkit';
 import vehiclesReducer from './vehiclesSlice.js';
+<<<<<<< Updated upstream
 import pedidosReducer from "./pedidosSlice.js"
+=======
+import ordersReducer from './ordersSlice.js';
+
+>>>>>>> Stashed changes
 
 const store = configureStore({
   reducer: { 
     vehicles: vehiclesReducer,
+<<<<<<< Updated upstream
     pedidos: pedidosReducer,
+=======
+    orders: ordersReducer,
+>>>>>>> Stashed changes
   },
 });
 
