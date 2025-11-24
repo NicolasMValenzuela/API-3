faa16d34f0cf5f33b1263898babfcb0ce941cdc1
Fran Reyna <franreyna2004@gmail.com>
2025-11-24 14:38:56 -0300

    todo en orden

--- Ecommerce-master/frontend/src/redux/store.js
+++ Ecommerce-master/frontend/src/redux/store.js
@@ -2,22 +2,16 @@ import {configureStore} from '@reduxjs/toolkit';
 import vehiclesReducer from './vehiclesSlice.js';
 import pedidosReducer from './pedidosSlice.js';
 import authReducer from "./authSlice.js";
-
-
-
-
-
-
 import cartReducer from './cartSlice.js';
-import ordersReducer from "./ordersSlice.js";
+import categoriesReducer from './categoriesSlice.js';
 
 export const store = configureStore({
   reducer: { 
     vehicles: vehiclesReducer,
     pedidos: pedidosReducer,
-    orders: ordersReducer,
     auth: authReducer,
     cart: cartReducer,
+    categories: categoriesReducer,
   },
 });
 export default store;
